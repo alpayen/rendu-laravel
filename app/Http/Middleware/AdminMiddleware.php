9edcb3ff9bd413a6d2dd816a3604cbb54266e8be
Adri Cdt <adrien.cadot@hotmail.fr>
2016-03-17 16:42:01 +0100

    Test routes

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
@@ -15,6 +16,11 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next)
     {
+
+        if(Auth::check() && Auth::user()->admin == 0 OR !Auth::check() ){
+            return redirect('home');
+        }
+
         return $next($request);
     }
 }
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app', ['pageTitle' => 'Admin panel'])
+
+@section('content')
+    <h2>Admin Panel!</h2>
+
+@endsection
