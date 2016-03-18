a0dd55c24fe6b0515fe28aef8b65d9a470d5aebf
Adri Cdt <adrien.cadot@hotmail.fr>
2016-03-18 17:29:45 +0100

    Home page pour Tobia

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,26 @@
 
 @section('content')
 
- @if(isset($message))
-<h2>{{$message}}</h2>
-@endif
- <h3>Welcome!</h3>
+    @if(isset($message))
+        <h2>{{$message}}</h2>
+    @endif
 
+    <div class="row text-center">
+        <h3 class="text-center">Bienvenue Alexandre.T</h3>
+
+        <div class="col-md-12 text-center" style="margin-bottom: 50px;">Tu pourras naviguer sur la plupart de nos créations via cette page.</div>
+
+        <div class="col-md-4">Pour nous envoyer un message, clique <strong><a href="{{route('contact.index')}}">ici</a></strong>
+            (Section Contact)
+        </div>
+        <div class="col-md-4">Pour voir les articles, clique <strong><a
+                        href="{{route('articles.index')}}">ici</a></strong> (Articles)
+        </div>
+        <div class="col-md-4">Pour accéder aux projets BAP, clique <strong><a href="{{route('bap.index')}}">ici</a></strong>
+            (Formulare pour soumettre un projet)
+        </div>
+
+
+    </div>
 
 @endsection
